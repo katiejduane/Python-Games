4a3570b847e827d31677a9c62aa46afcb5b8485a
Katie Duane <katiejduane@gmail.com>
2018-11-19 09:39:22 -0500

    Test

--- robs_txt_rpg_game.py
+++ robs_txt_rpg_game.py
@@ -4,5 +4,12 @@
 # 2. Dance
 # 3. Flee
 
+hero_name = raw_input("What is thy name, brave adventurer?")
+
 def fight():
+    print "Welcome %s. Thou art brave!" % hero_name
     print "Fight the goblin"
+    hero_health = 10
+    hero_power = 5
+    goblin_health = 6
+    goblin_power = 2
