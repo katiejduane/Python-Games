d2b362ce5899363d9f6be7fbfdd5d80c1099f868
Katie Duane <katiejduane@gmail.com>
2018-11-19 09:32:26 -0500

    Beginning of game files in Python

---
+++ my_txt_rpg_game.py
@@ -0,0 +1,4 @@
+# Text-Based RPG Game!
+# The is a procedural approach to a text based RPG game
+# There is a hero/protagonist
+# And he/she must have a few options that dictate how the game goes!
---
+++ robs_txt_rpg_game.py
@@ -0,0 +1,8 @@
+# Rob's example Goblin Game
+# The hero has the option to: 
+# 1. Fight
+# 2. Dance
+# 3. Flee
+
+def fight():
+    print "Fight the goblin"
